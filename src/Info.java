332200c99d161179c24e205bc7166de0d168e469
mail2yonz@gmail.com <mail2yonz@gmail.com>
2017-10-02 14:28:48 -0400

    Initial commit

---
+++ src/Info.java
@@ -0,0 +1,9 @@
+public class Info {
+
+    public static void main(String[] args)
+    {
+        //My Name
+        System.out.println("My name is yonatan Mengesha");
+        System.out.println("I am thirtythree years old");
+    }
+}
